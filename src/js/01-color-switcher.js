affa983f4f700cae566157ac320e41c56c3d8a5c
Yulia Kalashnyk <jkalashnik.97@gmail.com>
2023-04-13 22:41:32 +0300

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
+let idInt = null;
+
+const refs = {
+    btnStart: document.querySelector('button[data-start]'),
+    btnStop: document.querySelector('button[data-stop]'),
+    body: document.querySelector('body'),
+}
+
+refs.btnStop.disabled = true;
+refs.btnStart.addEventListener('click', onBtnStartChangeColor);
+refs.btnStop.addEventListener('click', onBtnStopChangeColor);
+
+
+function onBtnStartChangeColor() {
+    refs.btnStart.disabled = true;
+    refs.btnStop.disabled = false;
+
+    idInt = setInterval(() => {
+        refs.body.style.backgroundColor = getRandomHexColor()
+    }, 1000);
+}
+
+function onBtnStopChangeColor() {
+    refs.btnStart.disabled = false;
+    refs.btnStop.disabled = true;
+
+    clearInterval(idInt);
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+
 
